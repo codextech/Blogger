fa9a086024e4de1a06a54e4ea4a4463f56a6db26
codextech <tanzeelsaleem10@gmail.com>
2018-06-19 20:35:38 +0500

    Initial commit

---
+++ Blogger/Controllers/AdminPanelPostController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Blogger.Models;
+
+namespace Blogger.Controllers
+{
+    public class AdminPanelPostController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: AdminPanelPost
+        public ActionResult Index()
+        {
+            var postDetails = db.PostDetails.Include(p => p.CategoryDetail).Include(p => p.User);
+            return View(postDetails.ToList());
+        }
+
+        // GET: AdminPanelPost/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PostDetail postDetail = db.PostDetails.Find(id);
+            if (postDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(postDetail);
+        }
+
+        // GET: AdminPanelPost/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "CategoryName");
+            return View();
+        }
+
+        // POST: AdminPanelPost/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PostId,Title,Post_Content,Create_time,Tages,FeaturedImage,CategoryId,UserId")] PostDetail postDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PostDetails.Add(postDetail);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "CategoryName", postDetail.CategoryId);
+            return View(postDetail);
+        }
+
+        // GET: AdminPanelPost/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PostDetail postDetail = db.PostDetails.Find(id);
+            if (postDetail == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "CategoryName", postDetail.CategoryId);
+            return View(postDetail);
+        }
+
+        // POST: AdminPanelPost/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PostId,Title,Post_Content,Create_time,Tages,FeaturedImage,CategoryId,UserId")] PostDetail postDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(postDetail).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "CategoryName", postDetail.CategoryId);
+            return View(postDetail);
+        }
+
+        // GET: AdminPanelPost/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PostDetail postDetail = db.PostDetails.Find(id);
+            if (postDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(postDetail);
+        }
+
+        // POST: AdminPanelPost/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PostDetail postDetail = db.PostDetails.Find(id);
+            db.PostDetails.Remove(postDetail);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Blogger/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Blogger.Models;
+
+namespace Blogger.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController()
+        {
+            _context = new ApplicationDbContext();
+
+        }
+        // GET: Category
+        public PartialViewResult CategoryPartial()
+        {
+           var categories= _context.Categories.OrderBy(c=>c.CategoryName).ToList();
+            return PartialView(categories);
+        }
+
+
+        
+
+
+    }
+}
